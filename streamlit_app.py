ec580de3c675d282ee23062b46925f5bbf19a3f9
Marion Lee <141938627+MarionBml@users.noreply.github.com>
2025-04-14 16:02:35 +0000

    our team

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 
+
 intro_page = st.Page("1_intro.py", title="Introduction", icon="📖")
 data_page = st.Page("2_data.py", title="Data", icon="🗂️")
 
+
 pg = st.navigation([intro_page, data_page])
-st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
+st.set_page_config(page_title="Epilepsy project", page_icon=":material/edit:")
 pg.run()
+
+st.sidebar.title("Epilepsy project📄")
+with st.sidebar.expander("Our Team"):
+    st.caption("[Manon Boyer 🦸‍♀️](https://www.linkedin.com/in/manonboyerphd/)")
+    st.caption("[Paul Faucheux 🦸🏻‍♂️](https://www.linkedin.com/in/paul-faucheux-a876a741/)")
+    st.caption("[Marion Lee 🦸‍♀️](https://www.linkedin.com/in/marion-lee-b00731971/)")
