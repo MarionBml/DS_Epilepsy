c5a7a8bca756b2c83231dd1954b50799fb188d57
Marion Lee <141938627+MarionBml@users.noreply.github.com>
2025-04-16 14:45:12 +0000

    add pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,15 @@ import streamlit as st
 
 intro_page = st.Page("1_intro.py", title="Introduction", icon="📖")
 data_page = st.Page("2_donnees.py", title="Données", icon="🗂️")
+analysis_page = st.Page("3_analyse.py", title="Analyse", icon="📈")
+preproc_page = st.Page("4_preprocessing.py", title="Preprocessing", icon="⚙️")
+model_page = st.Page("5_modelisation.py", title="Modélisation", icon="⚖️")
+pred_page = st.Page("6_prediction.py", title="Prédiction", icon="🔮")
+conclu_page = st.Page("7_conclusion.py", title="Conclusion", icon="🥼")
+perspective_page = st.Page("8_perspective.py", title="Perspectives", icon="🔭")
 
 
-pg = st.navigation([intro_page, data_page])
+pg = st.navigation([intro_page, data_page, analysis_page, preproc_page, model_page, pred_page, conclu_page, perspective_page])
 st.set_page_config(page_title="Epilepsy project", page_icon=":material/edit:")
 pg.run()
 
