7cdad00f57f4e05621fcf8a8407eb0e6059d8a5b
Marion Lee <141938627+MarionBml@users.noreply.github.com>
2025-04-28 09:16:40 +0000

    thanks

--- 7_conclusion.py
+++ 7_conclusion.py
@@ -6,3 +6,11 @@ Lien entre les résultats obtenus et la problématique métier
 import streamlit as st
 
 st.title('Conclusion du projet')
+with st.sidebar.expander("Remerciements"):
+    st.markdown(""" À l'issue de notre projet, nous souhaitons exprimer notre gratitude envers les personnes qui ont soutenu notre travail. Leur contribution a été essentielle à la réalisation de cette étude.""")
+    st.markdown("""Ainsi, nous tenons tout particulièrement à remercier chaleureusement :""")                
+    st.markdown("""* 👨🏽‍⚕️ Dr Mario Chavez, à l'initiative de ce projet, pour sa généreuse fourniture des données anonymisées et annotées, indispensables à notre travail.""")
+    st.markdown("""* 👨🏻‍⚕️ Dr Valerio Frazzini, qui a récolté, sélectionné et annoté les données, assurant ainsi leur qualité et leur pertinence pour notre étude.""")
+    st.markdown("""* 👨🏻‍💻 Thomas Boehler, notre chef de cohorte et encadrant sur ce projet, pour sa guidance précieuse et son accompagnement lors de nos moments d'incertitude, en nous orientant vers les bonnes démarches.""")
+    st.markdown("""* 👩🏽‍💻 Aïda, notre Program Manager, pour la gestion de la planification des réunions sur la plateforme DataScientest, facilitant ainsi le bon déroulement de notre formation.""")
+    st.markdown("""Et enfin, un grand merci à l’ensemble de l’équipe de notre établissement de rattachement DataScientest.""")
